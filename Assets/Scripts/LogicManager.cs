89625b582fdcef6eaddd7d68cd2e059d7067912d
Maxime Bastien <maxbastien@hotmail.com>
2025-02-04 11:16:34 +0100

    Project files changed to other unity project

---
+++ Assets/Scripts/LogicManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
+
+public class LogicManager : MonoBehaviour
+
+{
+    [SerializeField] private GameObject gridObject;   // grid object containing the buttons
+    public Sprite[] buttonSprites;  // 4 sprites
+    private Button[] buttonsArray;  // Array to store buttons
+
+    [SerializeField] private GameObject correctImg;
+    public Button nextBtn;
+    public AudioManager audioManager;
+
+    void Start()
+    {
+        //***************************************Buttons Images  Randomize********************************************************************************************//
+        buttonsArray = gridObject.GetComponentsInChildren<Button>();
+
+        // Convert the sprite array into a List to track available sprites
+        List<Sprite> availableSprites = new List<Sprite>(buttonSprites);
+
+        // Assign each button a unique sprite
+        foreach (Button btn in buttonsArray)
+        {
+            Image childImage = btn.transform.Find("Image").GetComponent<Image>(); // Get child Image component
+
+            int randomIndex = Random.Range(0, availableSprites.Count); // Pick a random available sprite
+            childImage.sprite = availableSprites[randomIndex];  // Assign sprite
+
+            availableSprites.RemoveAt(randomIndex); // Remove from list to avoid duplicates
+
+            btn.onClick.AddListener(() => checkResponse(btn)); //add onClick event listener for each btn
+
+        }
+
+    }
+    //***************************************Game Logic **********************************************************************************************************//
+    private void checkResponse(Button clickedBtn)
+    {
+        string childImage = clickedBtn.transform.Find("Image").GetComponent<Image>().sprite.name;
+        Debug.Log("Source Image name:" + childImage);
+        if (childImage == correctImg.name)
+        {
+            correctImg.gameObject.SetActive(true);
+            audioManager.WinSound();
+            nextBtn.gameObject.SetActive(true);
+            foreach (Button btn in buttonsArray)
+            {
+                btn.interactable = false;
+            }
+        }
+        else
+        {
+            audioManager.Wrongound();
+        }
+    }
+}
